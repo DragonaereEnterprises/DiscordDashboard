301afa6ca5f74cbab09b10fde86945e5536e859d
Dragonaere <andrewdragonch@proton.me>
2024-01-21 15:42:39 -0500

    Fixed GraphQL - SSL + No port + Cloudflare Tunnel Still wanna figure out authentication

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -10,7 +10,7 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-    uri: "http://discordbotapi.dragonaere.com:4000/graphql/",
+    uri: "https://discordbotapi.dragonaere.com/",
     fetchOptions: { cache: "no-store" },
   });
 
