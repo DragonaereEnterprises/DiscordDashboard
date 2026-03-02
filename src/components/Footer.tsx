cab7eca6337c44f6e724e8b69dffcbb3c5cb91b6
Andrew Still <astill@dragonaere.com>
2026-03-02 07:32:45 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 Dragonaere Enterprises</p>
+      <p>© 2026 Dragonaere Enterprises</p>
     </footer>
   )
 }
